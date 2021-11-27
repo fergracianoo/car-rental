7ea9d7b25c21524d180fe2d63e0c6f013e0e2b44
Fernando Graciano <fernandoggraciano@gmail.com>
2021-11-27 01:00:37 -0300

    chore: create category type

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,15 +1,25 @@
 import { Router } from 'express';
 
+import { Category } from '../model/Category';
+
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
-categoriesRoutes.post('/categories', (req, res) => {
+categoriesRoutes.post('/', (req, res) => {
   const { name, description } = req.body;
 
-  categories.push({ name, description });
+  const category = new Category();
 
-  return res.status(201).send();
+  Object.assign(category, {
+    name,
+    description,
+    created_at: new Date(),
+  });
+
+  categories.push(category);
+
+  return res.status(201).json({ category });
 });
 
 export { categoriesRoutes };
